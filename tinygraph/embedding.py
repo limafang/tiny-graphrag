8b75e088c62065d73051dab85c60565c2c257c37
lta155 <1397298950@qq.com>
2024-09-26 17:25:12 +0800

    add local hf model

--- tinygraph/embedding.py
+++ tinygraph/embedding.py
@@ -1,5 +1,7 @@
 from zhipuai import ZhipuAI
 from typing import List
+from sentence_transformers import SentenceTransformer
+
 class zhipuemb:
     def __init__(self, api_key: str):
         self.client = ZhipuAI(api_key=api_key)
@@ -10,3 +12,10 @@ class zhipuemb:
             input=text,
         )
         return emb.data[0].embedding
+
+class Hf_Emb:
+    def __init__(self):
+        self.client = SentenceTransformer("embedding/models--Alibaba-NLP--gte-large-en-v1.5")
+    def get_emb(self, text:str) -> List[float]:
+        emb = self.client.encode(text)
+        return emb.tolist()
